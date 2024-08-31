0618229e7dd90bfe0357cd2d6c6ef70016340f72
mrman949 <infinitelives4ever@yahoo.com>
2024-08-31 01:18:37 -0500

    storeshot added to store images

--- Analysis/read.py
+++ Analysis/read.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
-
+import os
 mon = {'left': 0, 'top': 0, 'width': 3840, 'height': 2160}
 
 def screenshot():
@@ -14,4 +14,14 @@ def screenshot():
                 screenShot.rgb, 
             )
             cv2.destroyAllWindows()
-    return img
+    return np.array(img)
+
+#Takes an image like the one supplied by screenshot
+def storeshot(img):
+    currentDirectory = r'Analysis\storedImages'
+    print(os.listdir(currentDirectory))
+    latest = int((os.listdir(currentDirectory)[-1]).removesuffix('.jpg'))
+    name = latest + 1
+    cv2.imwrite(currentDirectory + "/" + str(name) + ".jpg", img)
+
+
